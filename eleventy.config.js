bbdf75653c53196e2bc722e16521c3a9410f19ce
Casey Watts <casey.s.watts@gmail.com>
2025-06-30 22:38:46 -0400

    set up eleventy site with ligtningcss (not working yet) and openprops(via cdn)

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,28 +1,29 @@
 import pluginWebc from "@11ty/eleventy-plugin-webc";
 import { InputPathToUrlTransformPlugin } from "@11ty/eleventy";
+import lightningCSS from "@11tyrocks/eleventy-plugin-lightningcss";
 
 /** @param {import('@11ty/eleventy').UserConfig} eleventyConfig */
-export default function(eleventyConfig) {
+export default function (eleventyConfig) {
 	eleventyConfig.ignores.add("README.md");
 
 	eleventyConfig.addPlugin(pluginWebc, {
-		components: [
-			"./_components/**/*.webc",
-			"npm:@11ty/is-land/*.webc"
-		]
+		components: ["./src/_components/**/*.webc", "npm:@11ty/is-land/*.webc"],
 	});
 
 	eleventyConfig.addPlugin(InputPathToUrlTransformPlugin);
 
 	eleventyConfig.setServerOptions({
-		domDiff: false
+		domDiff: false,
 	});
-};
+	eleventyConfig.addPlugin(lightningCSS);
+}
 
 export const config = {
 	dir: {
-		input: "content",          // default: "."
-		includes: "../_includes",  // default: "_includes"
-		data: "../_data",          // default: "_data"
-	}
+		input: "src",
+		output: "public",
+		// input: "content", // default: "."
+		// includes: "_includes", // default: "_includes"
+		// data: "../_data", // default: "_data"
+	},
 };
