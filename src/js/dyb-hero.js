54e35dbae5cbc84cb86c181c1e9174ee0a23df8c
Casey Watts <casey.s.watts@gmail.com>
2025-07-14 22:22:22 -0400

    hero component styling

--- src/js/dyb-hero.js
+++ src/js/dyb-hero.js
@@ -5,43 +5,29 @@ class DYBHero extends LitElement {
 		return html`
 			<style>
 				.dyb-hero {
-					background-color: rebeccapurple;
 					width: 100%;
-					min-height: var(--size-14);
+					background-color: rebeccapurple;
 					display: flex;
 					justify-content: space-evenly;
+					align-content: space-evenly;
 					align-items: center;
+					min-height: var(--size-14);
+					gap: var(--size-2);
+					flex-wrap: wrap;
+					padding: var(--size-3);
 					div {
 						background-color: var(--surface-1);
-						padding: var(--size-4);
-						max-width: var(--size-14);
-					}
-				}
-				.button-link {
-					display: inline-block;
-					padding: var(--size-3);
-					background-color: var(--surface-4);
-					color: var(--text-1);
-					text-decoration: none;
-
-					&:hover {
-						color: var(--text-2);
-						background-color: var(--surface-3);
+						padding: var(--size-6);
+						max-width: var(--size-15);
 					}
 				}
 			</style>
 			<div class="dyb-hero">
-				<div>
-					<h2>Debugging Your Brain</h2>
-					<p>
-						<strong>psychology</strong> techniques to rewire your brain using
-						<strong>software development</strong> metaphors
-					</p>
-					<a href="#" class="button-link">Buy Book</a>
+				<div class="panel">
+					<slot name="left"></slot>
 				</div>
-				<div>
-					<p>“Someone wrote the documentation I actually needed!"</p>
-					<p>— Sean Perkins, Software Developer</p>
+				<div class="panel">
+					<slot name="right"></slot>
 				</div>
 			</div>
 		`;
